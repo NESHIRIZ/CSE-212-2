c9feee61cd672b8a280c02160cafe56fc052511a
NESHIRIZ <tafadzwa.2000s@gmail.com>
2025-11-15 14:49:47 -0700

    done

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,24 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var wordSet = new HashSet<string>(words);
+        var results = new List<string>();
+
+        foreach (var word in words)
+        {
+            if (wordSet.Contains(word))
+            {
+                var reversed = new string(word.Reverse().ToArray());
+                if (word != reversed && wordSet.Contains(reversed))
+                {
+                    results.Add($"{word} & {reversed}");
+                    wordSet.Remove(word);
+                    wordSet.Remove(reversed);
+                }
+            }
+        }
+
+        return results.ToArray();
     }
 
     /// <summary>
@@ -42,10 +58,20 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            
+            if (fields.Length > 3)
+            {
+                var degree = fields[3].Trim();
+                if (degrees.ContainsKey(degree))
+                    degrees[degree]++;
+                else
+                    degrees[degree] = 1;
+            }
         }
 
         return degrees;
+
+        
     }
 
     /// <summary>
@@ -66,8 +92,29 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        if (word1.Length != word2.Length) return false;
+
+        var charCount = new Dictionary<char, int>();
+
+        foreach (var c in word1)
+        {
+            if (charCount.ContainsKey(c))
+                charCount[c]++;
+            else
+                charCount[c] = 1;
+        }
+
+        foreach (var c in word2)
+        {
+            if (!charCount.ContainsKey(c)) return false;
+            charCount[c]--;
+            if (charCount[c] < 0) return false;
+        }
+
+        return true;
     }
 
     /// <summary>
