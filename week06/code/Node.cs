7ffb3717c03149a381abd639da8091bca1696dd1
NESHIRIZ <tafadzwa.2000s@gmail.com>
2025-12-01 01:17:09 +0200

    DONE

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -9,21 +9,20 @@ public class Node
         this.Data = data;
     }
 
+    // Problem 1: Insert Unique Values Only
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data) return; // Prevent duplicates
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else // value > Data
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -31,15 +30,23 @@ public class Node
         }
     }
 
+    // Problem 2: Contains
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data) return true;
+
+        if (value < Data)
+            return Left?.Contains(value) ?? false;
+        else
+            return Right?.Contains(value) ?? false;
     }
 
+    // Problem 4: GetHeight
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
     }
 }
